684c9a15def542052679db20887226f75d60cb18
shahrokh <shahrokh.r1379@gmail.com>
2022-06-04 19:39:19 +0430

    admin-register model profile

--- jwitter/admin.py
+++ jwitter/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
-
+from .models import Profile
 
 class UserAdmin(admin.ModelAdmin):
     model = User
@@ -10,3 +10,4 @@ class UserAdmin(admin.ModelAdmin):
 admin.site.unregister(Group)
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+admin.site.register(Profile)
