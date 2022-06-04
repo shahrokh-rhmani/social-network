d6a7dca0bed5b62ce32f9e96cfc61c567e41ae8a
shahrokh <shahrokh.r1379@gmail.com>
2022-06-04 19:22:12 +0430

    unregister User & register User just fields username

--- jwitter/admin.py
+++ jwitter/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, User
+
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ('username',)
 
 
 admin.site.unregister(Group)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
