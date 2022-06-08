72869483bec23c2ade1bff90e6fe612283056cf1
shahrokh <shahrokh.r1379@gmail.com>
2022-06-08 23:17:50 +0430

    create views profile_list

--- jwitter/urls.py
+++ jwitter/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import dashboard
+from .views import dashboard, profile_list
 
 app_name= 'jwitter'
 
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
+    path('profile_list/', profile_list, 'profile_list'),
 ]
--- jwitter/views.py
+++ jwitter/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-
+from .models import Profile
 
 def dashboard(request):
     return  render(request, '_base.html')
+
+
+def profile_list(request):
+    profiles = Profile.objects.exclude(user=request.user)
+    context = {
+        'profiles':profiles,
+        
+    }
+    return render(reqeust, 'jwitter/profile_list.html', context)
