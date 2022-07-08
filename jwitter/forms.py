0e1219ad1056c7f802f99f9627935b4ddeda6738
shahrokh <shahrokh.r1379@gmail.com>
2022-07-08 20:33:39 +0430

    create class JweetForm in forms.py

---
+++ jwitter/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Jweet
+
+
+class JweetForm(forms.ModelForm):
+    body = forms.CharField(required=True)
+
+    class Meta:
+        model = Jweet
+         exclude = ("", )
