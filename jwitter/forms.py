dbc8bc2439f3b1590549617c0fc3179a4c523e7e
shahrokh <shahrokh.r1379@gmail.com>
2022-07-11 22:45:00 +0430

    add widget, Textarea to JweetForm,body

--- jwitter/forms.py
+++ jwitter/forms.py
@@ -3,7 +3,17 @@ from .models import Jweet
 
 
 class JweetForm(forms.ModelForm):
-    body = forms.CharField(required=True)
+    body = forms.CharField(
+        required=True,
+        widget=forms.widgets.Textarea(
+            attrs={
+                "placeholder":"Jweet somethings...",
+                "class":"textarea is-success is-medium",
+            }
+        ),
+        label="", 
+    )
+
 
     class Meta:
         model = Jweet
